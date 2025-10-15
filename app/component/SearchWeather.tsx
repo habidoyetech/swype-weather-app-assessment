b947fc8755927e398b35563a03a2c5e3305ab3d9
habidoyetech <abidoyeabiodun97@gmail.com>
2025-10-15 19:26:31 +0100

    remove tansack query

--- app/component/SearchWeather.tsx
+++ app/component/SearchWeather.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
 import { Button } from "@/components/ui/button";
 import InputSearch from "@/components/ui/search";
 import { toast } from "sonner";
